081870d3934b677ad0b3a488b2018207c9ada4bd
Dylan <Dylanraab1@usf.edu>
2026-05-04 22:21:30 -0400

    Fix missing EF Core packages, DbContext, and Item model

---
+++ CollectifyWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CollectifyWebApp.Models;
+
+namespace CollectifyWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add your DbSets here, e.g.:
+         public DbSet<Item> Items { get; set; }
+    }
+}
